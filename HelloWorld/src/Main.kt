97af71dea16d85a652beef11a34a0d97be2303a3
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-06-26 22:40:34 +0530

    Added the comments
    Added the types of comments in kotlin

--- HelloWorld/src/Main.kt
+++ HelloWorld/src/Main.kt
@@ -1,6 +1,10 @@
 fun main() {
-    // there is two types of the comments in kotlin Single line comments and multi-line comments
+    // There is two types of the comments in kotlin Single line comments and multi-line comments
     // This is the single line comment.
+    /*
+    This is a
+    multi line comment
+     */
     //fun is keyword in kotlin to create the function
     // main is the entry point of the program where the program begin its execution
 
