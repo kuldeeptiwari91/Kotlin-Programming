99fe47e8d8057c1b6724f690c7de4e047222de97
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-07-02 17:13:20 +0530

    Update Main.kt

--- if-else/src/Main.kt
+++ if-else/src/Main.kt
@@ -6,7 +6,7 @@ fun main() {
     // When the condition is satisfied, the if part is executed.
     // If the condition is not satisfied, then the else part is executed.
     // In this example, if the number is less than 0, the if part is executed.
-    // The else if part is executed if its condition is true.
+    // The else if part is executed if its condition is true, which is number is greater than 0
     // The else part is executed only when all the other conditions are false.
     if (myNumber < 0) {
         println("The number is negative")
