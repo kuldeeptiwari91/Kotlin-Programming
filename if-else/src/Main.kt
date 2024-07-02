1574d7c641ec499f3bb1f1c0ce2250eed3b39647
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-07-02 17:11:32 +0530

    if-else
    if-else in kotlin

---
+++ if-else/src/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Enter your number")
+    val myNumber = readln().toInt()
+
+    // if-else is the conditional expression in Kotlin.
+    // When the condition is satisfied, the if part is executed.
+    // If the condition is not satisfied, then the else part is executed.
+    // In this example, if the number is less than 0, the if part is executed.
+    // The else if part is executed if its condition is true.
+    // The else part is executed only when all the other conditions are false.
+    if (myNumber < 0) {
+        println("The number is negative")
+    } else if (myNumber > 0) {
+        println("The number is positive")
+    } else {
+        println("The number is zero")
+    }
+}
