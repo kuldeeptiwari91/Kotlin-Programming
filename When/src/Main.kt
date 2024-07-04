fc00f666b8698df1c340cfabf288e6a896cd2098
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-07-04 21:38:13 +0530

    When In kotlin
    When In kotlin

---
+++ When/src/Main.kt
@@ -0,0 +1,18 @@
+import java.util.*
+
+fun main() {
+    println("Enter the Day!")
+    val day: String = readln().lowercase(Locale.getDefault())
+    // 'when' is a conditional expression in Kotlin. It is similar to the switch-case in Java.
+    // It is used to simplify similar conditions.
+    when (day) {
+        "monday" -> println("It's Monday")
+        "tuesday" -> println("It's Tuesday")
+        "wednesday" -> println("It's Wednesday")
+        "thursday" -> println("It's Thursday")
+        "friday" -> println("It's Friday")
+        "saturday" -> println("It's Saturday")
+        "sunday" -> println("It's Sunday")
+        else -> println("Invalid day")
+    }
+}
