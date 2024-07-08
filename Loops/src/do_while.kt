b96e856196e4fa8d59b647324e3aca605c578b6f
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-07-08 23:43:52 +0530

    Update do_while.kt

--- Loops/src/do_while.kt
+++ Loops/src/do_while.kt
@@ -1,3 +1,11 @@
-fun main(){
-
+fun main() {
+    val number = 10
+    // 'do-while' is another way to create loops in Kotlin.
+    // The 'do-while' loop will execute the block of code once, even if the condition is false.
+    do {
+        println(number)
+        // In this case, 'number' is printed once, even though the condition is false.
+    } while (number < 10)
+    // After the first execution, the condition is checked.
+    // Since 'number < 10' is false, the loop terminates.
 }
