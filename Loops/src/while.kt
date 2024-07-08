160d1e16fbf2e76048811b87443e79725fb4608f
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-07-08 23:39:49 +0530

    Update the for and while

--- Loops/src/while.kt
+++ Loops/src/while.kt
@@ -1,3 +1,15 @@
-fun main(){
+fun main() {
+    // 'while' is another way to create loops in Kotlin.
+    // The 'while' loop checks the condition before executing the loop's body.
+    // If the condition is true, it executes the loop's body; if false, it exits the loop.
 
+    var number = 0
+
+    // This 'while' loop will print numbers from 0 to 9.
+    while (number < 10) {
+        println(number)
+        number++
+        // The number variable is incremented by 1 in each iteration.
+        // The loop continues until 'number' is no longer less than 10.
+    }
 }
