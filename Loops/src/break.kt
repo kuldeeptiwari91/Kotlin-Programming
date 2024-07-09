3b5652d6a50fe30ff43c4da12b9b89ee427b2629
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-07-09 21:02:03 +0530

    added the continue and break
    added the continue and break

---
+++ Loops/src/break.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var number = 0
+
+    // 'while' loop checks the condition before each iteration.
+    while (number < 10) {
+        number++
+
+        // If the number equals 7, the loop will break.
+        if (number == 7) {
+            // 'break' is used to exit the loop when the condition is satisfied.
+            break
+        }
+
+        // Print the current value of number.
+        println(number)
+    }
+}
