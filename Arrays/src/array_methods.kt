b33d2769d6ffbb88bf957926a8de0dcba91c0200
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-07-11 21:00:30 +0530

    Update array_methods.kt

--- Arrays/src/array_methods.kt
+++ Arrays/src/array_methods.kt
@@ -3,5 +3,6 @@ fun main() {
     // Array method are inbuilt function which is provided by the kotlin compiler. There is different methods some ot them are:
     // size method:- this method return the size of array
     println(numbers.size)
-    // 
+    // get(index: Int) :- this method is used to get the value at particular index
+    println(numbers.get(4))
 }
