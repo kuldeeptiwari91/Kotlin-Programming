e701d0cc39d92b0ffc741517202af2294b2700e3
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-07-11 20:54:26 +0530

    Create array_methods.kt

---
+++ Arrays/src/array_methods.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val numbers = arrayOf(1, 2, 3, 4, 5, 6)
+    // Array method are inbuilt function which is provided by the kotlin compiler. There is different methods some ot them are:
+    // size method:- this method return the size of array
+    println(numbers.size)
+    // 
+}
