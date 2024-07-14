42f190bc5fe1e776246c448cfe8ec9cc89d2fd6f
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-07-14 22:11:30 +0530

    Update primary_constructor.kt

--- OOP/src/primary_constructor.kt
+++ OOP/src/primary_constructor.kt
@@ -1,23 +1,33 @@
+// This is a Kotlin program demonstrating the use of primary constructors in a class.
+
 fun main() {
+    // Using the primary constructor to create an object of the Car1 class
     val car2 = Car1("Tesla", "XY1", "Black", 4)
 
+    // Printing the properties of car2
     println("Name = ${car2.name}")
     println("Model = ${car2.model}")
     println("Color = ${car2.color}")
     println("Doors = ${car2.doors}")
 
+    // Calling the methods of car2
     car2.move()
     car2.stop()
 }
 
+// Class Car1 with a primary constructor
 class Car1(var name: String, var model: String, var color: String, var doors: Int) {
+    // Class properties are defined in the primary constructor
+    // var name: String, var model: String, var color: String, var doors: Int
+    // These properties are initialized with the values passed when the object is created.
 
+    // Method to simulate the car moving
     fun move() {
         println("The car $name is moving")
     }
 
+    // Method to simulate the car stopping
     fun stop() {
         println("The car $name has stopped")
     }
-
 }
