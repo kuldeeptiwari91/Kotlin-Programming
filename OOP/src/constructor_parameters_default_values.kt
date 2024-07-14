236a49171f44e4259413bde7c728aa1099170bef
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-07-14 22:22:43 +0530

    Added Comments to files
    Added Comments to files

--- OOP/src/constructor_parameters_default_values.kt
+++ OOP/src/constructor_parameters_default_values.kt
@@ -1,8 +1,19 @@
 fun main() {
-    val user = UsersDetails("Cillian")
-    val user1 = UsersDetails("Lillian", "Murphy", 28)
+    // Creating objects of the UsersDetails class using both primary and secondary constructors
+    val user = UsersDetails("Cillian") // Uses default values for lastName and age
+    val user1 = UsersDetails("Lillian", "Murphy", 28) // Provides values for all parameters
 }
 
+// Class UsersDetails with a primary constructor that has default values for lastName and age
 class UsersDetails(var firstName: String, var lastName: String = "LastName", var age: Int = 0) {
+    /*
+    Explanation:
 
+         Primary Constructor with Default Values:
+       - The class UsersDetails is defined with a primary constructor that has three parameters:
+         firstName, lastName, and age.
+       - The parameters lastName and age have default values of "LastName" and 0, respectively.
+       - This means that if these parameters are not provided when creating an object, the default values will be used.
+
+    */
 }
--- OOP/src/secondary_constructors.kt
+++ OOP/src/secondary_constructors.kt
@@ -8,13 +8,15 @@ fun main() {
 class Users(var name: String, var lastName: String, var age: Int) {
     // Secondary constructor: Provides an alternative way to create objects
     // Calls the primary constructor with default values for lastName and age
-    constructor(name: String) : this(name, "LastName", 0)
-}
 
-/*
+    /*
      Secondary Constructor:
    - The secondary constructor provides an alternative way to create objects of the `Users` class.
    - It takes one parameter, `name`, and calls the primary constructor with default values for `lastName` ("LastName") and `age` (0).
    - The `this` keyword is used to delegate to the primary constructor.
    - This allows for more flexibility in object creation, providing default values when not all parameters are provided.
 */
+
+    constructor(name: String) : this(name, "LastName", 0)
+}
+
