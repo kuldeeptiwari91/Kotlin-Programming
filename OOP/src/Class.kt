d2801c19bea8ec2a024fff3e82e00be0c7ad3cef
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-07-14 22:04:55 +0530

    Update Class.kt

--- OOP/src/Class.kt
+++ OOP/src/Class.kt
@@ -1,31 +1,45 @@
+// A class is a blueprint for creating objects.
+// It can contain properties (variables) and methods (functions) to define the behavior of the objects created from the class.
+
 fun main() {
+    // Creating an object of the Car class using the primary constructor
+    // The syntax for creating an object is:
+    // val objectName = ClassName()
+    // Here, car1 is the object of the Car class
     val car1 = Car()
+
+    // Setting properties of the car1 object
     car1.name = "Tesla"
     car1.model = "XY!"
     car1.color = "black"
     car1.doors = 4
 
+    // Printing properties of the car1 object
     println("Name = ${car1.name}")
     println("Model = ${car1.model}")
     println("Color = ${car1.color}")
     println("Doors = ${car1.doors}")
 
+    // Calling the methods of the car1 object
     car1.move()
     car1.stop()
 }
 
+// Define a class named Car
 class Car {
-    var name = ""
-    var model = ""
-    var color = ""
-    var doors = 0
+    // Properties of the class
+    var name = "" // Name of the car
+    var model = "" // Model of the car
+    var color = "" // Color of the car
+    var doors = 0 // Number of doors in the car
 
+    // Method to simulate the car moving
     fun move() {
         println("The car $name is moving")
     }
 
+    // Method to simulate the car stopping
     fun stop() {
         println("The car $name has stopped")
     }
-
 }
