abfca99e2194b62841ab694c5936e8421eb00c8c
Kuldeep Tiwari <95047794+kuldeeptiwari91@users.noreply.github.com>
2024-07-19 21:52:35 +0530

    Added the comments

--- Inheritance/src/data_class.kt
+++ Inheritance/src/data_class.kt
@@ -1,11 +1,20 @@
 fun main() {
+    // Creating two instances of the User data class with the same values
     val user1 = User("James", "Bond", 24)
     val user2 = User("James", "Bond", 24)
-    println(user1 == user2)
-    println(user2 == user1)
-    println(user1)
+
+    // Checking if user1 is equal to user2 using the '==' operator
+    println(user1 == user2) // Prints 'true' because data classes automatically generate an equals() method
+
+    // Checking if user2 is equal to user1 using the '==' operator
+    println(user2 == user1) // Prints 'true' because data classes automatically generate an equals() method
+
+    // Printing user1 using the automatically generated toString() method of the data class
+    println(user1) // Prints 'User(firstName=James, lastName=Bond, age=24)'
 }
 
+// This is the commented out version of the User class
+// It manually implements equals(), hashCode(), and toString() methods
 //class User(var firstName: String, var lastName: String, var age: Int) {
 //    override fun equals(other: Any?): Boolean {
 //        if (this === other) {
@@ -22,9 +31,10 @@ fun main() {
 //    }
 //
 //    override fun toString(): String {
-//        return "User(firstName = '$firstName'  lastName = '$lastName' age = '$age')"
+//        return "User(firstName = '$firstName', lastName = '$lastName', age = '$age')"
 //    }
 //}
 
-data class User(var firstName: String, var lastName: String, var age: Int) {
-}
+// This is the data class version of the User class
+// Data classes automatically provide implementations for equals(), hashCode(), and toString() methods
+data class User(var firstName: String, var lastName: String, var age: Int)
--- Inheritance/src/interface.kt
+++ Inheritance/src/interface.kt
@@ -1,21 +1,34 @@
 fun main() {
+    // Creating instances of Car and Truck and calling their startEngine method
+    val myCar = Car1("Tesla", "Red")
+    myCar.startEngine() // This will print: "The Tesla car is starting engine!"
 
+    val myTruck = Truck("Ford", "Blue")
+    myTruck.startEngine() // This will print: "The Ford truck is starting engine!"
 }
 
-
+// Interface definition in Kotlin
+// An interface is a blueprint for classes that defines a set of functions and properties that the classes must implement.
 interface Engine {
+    // Abstract function declaration
+    // Any class that implements this interface must provide a concrete implementation of this function.
     fun startEngine()
 }
 
-class car(var name: String, var color: String) : Engine {
+// Class Car implementing the Engine interface
+// The Car class must provide an implementation for the startEngine function.
+class Car1(var name: String, var color: String) : Engine {
+    // Providing the implementation for the startEngine function as required by the Engine interface
     override fun startEngine() {
         println("The $name car is starting engine!")
     }
 }
 
+// Class Truck implementing the Engine interface
+// The Truck class must provide an implementation for the startEngine function.
 class Truck(var name: String, var color: String) : Engine {
+    // Providing the implementation for the startEngine function as required by the Engine interface
     override fun startEngine() {
         println("The $name truck is starting engine!")
     }
-
 }
